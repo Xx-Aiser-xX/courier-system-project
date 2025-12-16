7bba0ebf4a5bc9ebdb5555c4214447617d42fa39
Xx-Aiser-xX <stas.plot@mail.ru>
2025-12-16 04:24:22 +0300

    add test

--- couriers/src/test/java/org/example/couriers/CouriersApplicationTests.java
+++ couriers/src/test/java/org/example/couriers/CouriersApplicationTests.java
@@ -1,13 +1,17 @@
 package org.example.couriers;
 
+import org.example.couriers.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CouriersApplicationTests {
 
     @Test
-    void contextLoads() {
+    void oneEqualsOne() {
+        assertEquals(1, 1);
     }
 
 }
